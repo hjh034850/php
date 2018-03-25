c5ca53b8b219d5a3b18c38cc924ae8494ed7b19e
hjh034850 <hujihuimo@163.com>
2018-03-25 18:42:26 +0800

    all.php

---
+++ blog/del.php
@@ -0,0 +1,14 @@
+<?php
+include "conn.php";
+
+if(isset($_GET['id'])){
+	$id = $_GET['id'];
+	$sql  = "delete from blog where bid = '$id'";
+	$query = mysqli_query($link,$sql);
+	if($query){
+		header('location:index.php');
+	}else{
+		echo "<script>alert('删除失败!')</script>";
+	}
+}
+?>
